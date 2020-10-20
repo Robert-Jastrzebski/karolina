a84bd24307d232aca40e84bc7b1bd8e0660dd810
Robert Jastrzębski <macbookpro@MacBook-Pro-Muza.local>
2020-10-20 15:36:57 +0200

    Add About Section

--- main.js
+++ main.js
@@ -3,6 +3,11 @@ $(".nav-link").click(function () {
 });
 
 
-// $(".nav-link").click = () => {
-//     $('#mainmenu').removeClass('show');
-// };
+// Back to top button
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+        $('.back-to-top').fadeIn('slow');
+    } else {
+        $('.back-to-top').fadeOut('slow');
+    }
+});
